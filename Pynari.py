701dbdd2211afd1d1596d594cb06a6346e6d5b7b
mrdannyarmstrong <98234578+mrdannyarmstrong@users.noreply.github.com>
2022-02-09 15:46:58 -0500

    Add files via upload

---
+++ Pynari.py
@@ -0,0 +1,14 @@
+import random
+def main():
+    running = 1
+    while (running == 1):
+        bit1 = str(random.randrange(2))
+        bit2 = str(random.randrange(2))
+        bit3 = str(random.randrange(2))
+        bit4 = str(random.randrange(2))
+        bit5 = str(random.randrange(2))
+        bit6 = str(random.randrange(2))
+        bit7 = str(random.randrange(2))
+        bit8 = str(random.randrange(2))
+        print(bit1 + bit2 + bit3 + bit4 + bit5 + bit6 + bit7 + bit8)
+main()
